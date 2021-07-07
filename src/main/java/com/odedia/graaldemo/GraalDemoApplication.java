9a524735cebd58ca7501405eb6dca4d43a5c8a6b
Oded Shopen <odedia@gmail.com>
2021-07-07 08:44:30 +0300

    first commit

---
+++ src/main/java/com/odedia/graaldemo/GraalDemoApplication.java
@@ -0,0 +1,13 @@
+package com.odedia.graaldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odedia/graaldemo/GraalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odedia.graaldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
